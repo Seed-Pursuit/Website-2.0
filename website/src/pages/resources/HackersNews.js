55478b00d205e73b6a8d8eb4611bfc0f1c5a646a
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-24 20:00:50 +0530

    news

--- website/src/pages/resources/HackersNews.js
+++ website/src/pages/resources/HackersNews.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const HackersNews = () => {
-  return (
-    <div>HackersNews</div>
-  )
-}
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default HackersNews
+  useEffect(() => {
+    const apiKey = 'd6ffc9b586ad4e46944db8aa0a7c2922';
+    const apiUrl = `https://newsapi.org/v2/everything?q=startup&apiKey=${apiKey}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setNews(data.articles);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching news:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="p-20">
+      <h1 className="text-4xl text-black mb-4">Startup News</h1>
+      {loading ? (
+        <p className="text-lg text-black">Loading...</p>
+      ) : (
+        <div>
+          {news.map((article, index) => (
+            <div key={index} className="mb-4">
+              <h2 className="text-xl text-black">{article.title}</h2>
+              <p className="text-gray-500">{article.description}</p>
+              <a href={article.url} className="text-blue-600 hover:underline">
+                Read more
+              </a>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HackersNews;
