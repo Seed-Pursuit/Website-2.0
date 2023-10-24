7c519ae34260319957dcc2f596ba6776bc8371a6
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-25 04:45:48 +0530

    updated

--- website/src/pages/find_a_co_founder/StartupSchool.js
+++ website/src/pages/find_a_co_founder/StartupSchool.js
@@ -1,10 +1,96 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import avatar1 from '../../assets/avatar1.png';
+import avatar2 from '../../assets/avatar2.png';
+import avatar3 from '../../assets/avatar3.png';
+import Search from '../../assets/Search.png';
+import { Link } from 'react-router-dom';
 
 const StartupSchool = () => {
     return (
-        <div>
-            <Sidebar/>
+        <div className='flex'>
+            <Sidebar />
+            <div>
+                <p className="text-[45px] text-left text-black p-20">Co-founder Matching Dashboard</p>
+                <div>
+                    <div className="w-[389px] h-[511px]">
+                        <div className="w-[389px] h-[511px] absolute left-[342.5px] top-[295.5px] rounded-[10px] bg-[#f5f5ee] border border-black" />
+                        <div className="w-[273px] h-[98px]">
+                            <img
+                                className="w-[76px] h-[98px] absolute left-[400.5px] top-[376.5px] rounded-lg"
+                                src={avatar1}
+                                alt=''
+                            />
+                            <img
+                                className="w-[77px] h-[98px] absolute left-[498.5px] top-[376.5px] rounded-lg"
+                                src={avatar2}
+                                alt=''
+                            />
+                            <img
+                                className="w-[76px] h-[98px] absolute left-[597.5px] top-[376.5px] rounded-lg"
+                                src={avatar3}
+                                alt=''
+                            />
+                        </div>
+
+                        <p className="absolute left-[524px] top-[508px] text-[45px] text-left text-black">3</p>
+                        <p className="absolute left-[428px] top-[564px] text-[35px] text-left text-black">
+                            Pending requests
+                        </p>
+                        <p className="absolute left-[429px] top-[613px] text-lg text-left text-black">
+                            awaiting your response
+                        </p>
+                        <div className="w-[216px] h-[61px] absolute left-[427px] top-[676px]">
+                            <div className="w-[216px] h-[61px] absolute left-[-1px] top-[-1px] rounded-tl-[20px] rounded-br-[20px] bg-[#fb7a5a]" />
+                        </div>
+                        <div className="w-[216px] h-[61px] absolute left-[427px] top-[676px]">
+                            <div className="w-[216px] h-[61px] absolute left-[-1px] top-[-1px] rounded-tl-[20px] rounded-br-[20px] bg-[#fb7a5a]" />
+                        </div>
+                        <Link to="/inbox">
+                            <p className="absolute left-[513px] top-[693px] text-lg font-bold text-left text-white">View</p>
+                        </Link>
+                    </div>
+                    <div className="w-[389px] h-[511px]">
+                        <div className="w-[389px] h-[511px] absolute left-[776.5px] top-[295.5px] rounded-[10px] bg-[#f5f5ee] border border-black" />
+                        <img
+                            className="w-[77px] h-[98px] absolute left-[932.5px] top-[376.5px] rounded-lg"
+                            src={avatar1} alt=''
+                        />
+                        <p className="absolute left-[958px] top-[508px] text-[45px] text-left text-black">1</p>
+                        <p className="absolute left-[862px] top-[564px] text-[35px] text-left text-black">
+                            Unread message
+                        </p>
+                        <p className="absolute left-[863px] top-[613px] text-lg text-left text-black">
+                            awaiting your response
+                        </p>
+                        <div className="w-[216px] h-[61px] absolute left-[861px] top-[676px]">
+                            <div className="w-[216px] h-[61px] absolute left-[-1px] top-[-1px] rounded-tl-[20px] rounded-br-[20px] bg-[#fb7a5a]" />
+                        </div>
+                        <Link to="/inbox">
+                            <p className="absolute left-[947px] top-[693px] text-lg font-bold text-left text-white">View</p>
+                        </Link>
+                    </div>
+                    <div className="w-[823px] h-[247px]">
+                        <div className="w-[823px] h-[247px] absolute left-[342.5px] top-[844.5px] rounded-[10px] bg-[#f9fbf2] border border-black" />
+                        <p className="absolute left-[447px] top-[896px] text-[35px] text-left text-black">
+                            5000 founders in your queue meet your requirement!
+                        </p>
+                        <img
+                            src={Search}
+                            alt=''
+                            className="w-[79px] h-[68px] absolute left-[376.5px] top-[890.5px] object-contain"
+                        />
+                        <div className="w-[216px] h-[61px] absolute left-[624px] top-[998px]">
+                            <div className="w-[216px] h-[61px] absolute left-[-1px] top-[-1px] rounded-tl-[20px] rounded-br-[20px] bg-[#fb7a5a]" />
+                        </div>
+                        <Link to="/inbox">
+                            <p className="absolute left-[673px] top-[1010px] text-lg font-bold text-left text-white">
+                                View Profiles
+                            </p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- website/src/pages/startup_jobs/StartupJobs.js
+++ website/src/pages/startup_jobs/StartupJobs.js
@@ -1,9 +1,109 @@
-import React from 'react'
+import React from 'react';
 
 const StartupJobs = () => {
-  return (
-    <div>StartupJobs</div>
-  )
-}
+  const startupJobs = [
+    {
+      id: 1,
+      title: 'Frontend Developer',
+      company: 'TechStartup Inc.',
+      location: 'San Francisco, CA',
+      description: 'We are looking for a talented Frontend Developer to join our innovative team...',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 2,
+      title: 'Product Manager',
+      company: 'StartupX',
+      location: 'New York, NY',
+      description: 'StartupX is seeking a Product Manager to lead the development of our new product line...',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 3,
+      title: 'UX Designer',
+      company: 'GrowthTech',
+      location: 'Seattle, WA',
+      description: 'GrowthTech is hiring a UX Designer who is passionate about creating exceptional user experiences...',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 4,
+      title: 'Software Engineer',
+      company: 'InnovateTech Solutions',
+      location: 'Austin, TX',
+      description: 'Join our dynamic team as a Software Engineer and work on cutting-edge projects.',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 5,
+      title: 'Marketing Manager',
+      company: 'Digital Ventures',
+      location: 'Los Angeles, CA',
+      description: 'Digital Ventures is looking for a Marketing Manager to lead our marketing efforts.',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 6,
+      title: 'Data Analyst',
+      company: 'InfoTech Innovators',
+      location: 'Boston, MA',
+      description: 'InfoTech Innovators is seeking a Data Analyst to analyze and interpret data.',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 7,
+      title: 'Mobile App Developer',
+      company: 'AppWorks Co.',
+      location: 'San Diego, CA',
+      description: 'Join AppWorks Co. as a Mobile App Developer and create innovative mobile applications.',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 8,
+      title: 'Sales Associate',
+      company: 'SalesPro Inc.',
+      location: 'Chicago, IL',
+      description: 'SalesPro Inc. is hiring a Sales Associate to drive our sales efforts.',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 9,
+      title: 'Content Writer',
+      company: 'ContentCreators Ltd.',
+      location: 'Denver, CO',
+      description: 'ContentCreators Ltd. is looking for a talented Content Writer to create engaging content.',
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 10,
+      title: 'Network Administrator',
+      company: 'TechNet Solutions',
+      location: 'Houston, TX',
+      description: 'TechNet Solutions is seeking a Network Administrator to manage our network infrastructure.',
+      image: 'https://via.placeholder.com/150',
+    },
+  ];
 
-export default StartupJobs
+  return (
+    <div>
+    <h1 className="text-3xl md:text-5xl text-left text-black p-20">Startup Jobs</h1>
+    <div className="jobs p-4">
+      {startupJobs.map((job) => (
+        <div key={job.id} className="border border-gray-300 rounded-md p-4 mb-4 flex items-center">
+          <img src={job.image} alt={job.company} className="w-16 h-16 rounded-full mr-4" />
+          <div>
+            <h2 className="text-xl font-semibold text-black">{job.title}</h2>
+            <p className="text-gray-500">{job.company} - {job.location}</p>
+            <p className="text-black">{job.description}</p>
+            <button className="bg-[#84DCCF] text-white px-4 py-2 rounded-md mt-2">
+              Apply
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+  );
+};
+
+export default StartupJobs;
